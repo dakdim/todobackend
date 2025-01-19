973f1abe475202dda7e72a351b9b69e7deffae21
Shailesh Devkota <devkotashailesh76@gmail.com>
2025-01-19 13:55:34 +0545

    slightly change in add host

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gkcxy1v+n_74b)!c-z$5=*hb!l$jf7m#)_1g&a1xvngw#%9hpc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.82','127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.82','127.0.0.1','10.0.2.2']
 
 
 # Application definition
